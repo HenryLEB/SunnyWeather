d0d9393cec2ad0708f14702249ddf21765ab3c5b
HenryLEB <1205543030@qq.com>
2020-12-26 09:15:57 +0800

    test commit

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.sunnyweather
+package com.sunnyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/sunnyweather/android/ExampleUnitTest.kt
+++ app/src/test/java/com/sunnyweather/android/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.sunnyweather
+package com.sunnyweather.android
 
 import org.junit.Test
 
