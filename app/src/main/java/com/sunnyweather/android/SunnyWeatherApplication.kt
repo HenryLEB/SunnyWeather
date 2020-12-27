67e837cdadc10d181244900c1b0b4511b85df039
HenryLEB <1205543030@qq.com>
2020-12-27 22:05:52 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,20 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+
+        const val TOKEN = "ab6E0r6TaHqSJe8d"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
