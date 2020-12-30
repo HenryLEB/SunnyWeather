2fb7229b3254a765952eeb662b2ffa15e84f55fe
HenryLEB <1205543030@qq.com>
2020-12-30 18:17:23 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 
-class SunnyWeatherApplication : Application() {
+class   SunnyWeatherApplication : Application() {
 
     companion object {
         @SuppressLint("StaticFieldLeak")
